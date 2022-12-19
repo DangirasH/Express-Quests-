7150ccecba246eebbdbab5078fdef6efdfe45da3
Dangiras <dan.hausmans@gmail.com>
2022-12-19 11:57:23 +0100

    hash

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
-
 const port = 5000;
+app.use(express.json());
+const { validateMovie, validateUser } = require("./validators.js");
+const usersHandlers = require("./usersHandlers");
+const movieHandlers = require("./movieHandlers");
+
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -10,10 +14,19 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
-
+// Movies
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie)
+
+// Users
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
+app.post("/api/users", validateUser, usersHandlers.postUser);
+app.put("/api/users/:id", usersHandlers.updateUser);
+app.delete("/api/users", usersHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
---
+++ usersHandlers.js
@@ -0,0 +1,108 @@
+const database = require("./database");
+
+const getUsers = (req, res) => {
+  let sql = "select * from users";
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+  }
+  if (req.query.city != null) {
+    sql += " where city <= ?";
+    sqlValues.push(req.query.city);
+  }
+
+  database
+    .query(sql, sqlValues)
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      console.log(result);
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving users");
+    });
+};
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing user");
+    });
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the users");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUsersById,
+  postUser,
+  updateUser,
+  deleteUser,
+};
